a563c87756ccaf01db32c00768e861256988b7ce
Mittranj <mpansuriya@unomaha.edu>
2020-12-25 14:23:54 -0600

    added styles to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
+import Details from "./components/Details/Details";
+import Main from "./components/Main/Main";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <div className="app">
-      <h1>Hello MJ</h1>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
     </div>
   );
 };
